76a5ddb70aa8b8ab4f7579b15a8fb0bf29e19b21
rodrigoacosta46 <roroacostacant@gmail.com>
2024-10-21 20:58:08 -0300

    commit inicial

---
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/send")
+    @SendTo("/topic/messages")
+    public String sendMessage(String message) {
+        return message; // Aquí puedes procesar el mensaje
+    }
+}
